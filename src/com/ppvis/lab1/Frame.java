e3e3c85ae4186705f7cb9189f3dc29cde4a1fc02
NaKiR <hakup@tut.by>
2015-02-18 20:33:21 +0300

    1 commit

--- src/com/ppvis/lab1/Frame.java
+++ src/com/ppvis/lab1/Frame.java
@@ -5,25 +5,25 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by NotePad on 17.02.2015.
  */
 
 public class Frame extends JFrame {
-    private ArrayList<String> comboBoxItems = new ArrayList<String>();
+    private String[] comboBoxItems = new String[] {"string1"};
     private JButton button = new JButton("Add");
-    private JTextField input = new JTextField("", 5);
+    private JTextField input = new JTextField("", 10);
     private JLabel label = new JLabel("Input:");
-    private JComboBox comboBox = new JComboBox(comboBoxItems.toArray());
+    private JComboBox comboBox = new JComboBox(comboBoxItems);
 
     public Frame() {
         super("PPVIS lab 1");
         this.setBounds(100, 100, 200, 60);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        comboBoxItems.add("LALKA");
         Container container = this.getContentPane();
-        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
         container.add(label);
         container.add(input);
         button.addActionListener(new ButtonEventListener());
@@ -34,19 +34,18 @@ public class Frame extends JFrame {
     class ButtonEventListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
+            boolean ifExist = false;
             String newItem = input.getText();
-            System.out.println(newItem);
-            if (comboBoxItems.size() != 0) {
-                for (int itemIndex = 0; itemIndex < comboBoxItems.size(); itemIndex++) {
-                    if (comboBoxItems.get(itemIndex) == newItem) {
-                        JOptionPane.showMessageDialog(null, "Данная строка уже включена.",
-                                "Output", JOptionPane.PLAIN_MESSAGE);
-                    } else {
-                        comboBoxItems.add(newItem);
-                    }
+            for (int itemIndex = 0; itemIndex < comboBox.getItemCount(); itemIndex++) {
+                if (comboBox.getItemAt(itemIndex).equals(newItem)) {
+                    ifExist = true;
                 }
+            }
+            if (ifExist) {
+                JOptionPane.showMessageDialog(null, "Данная строка уже включена.",
+                        "Error!", JOptionPane.PLAIN_MESSAGE);
             } else {
-                comboBoxItems.add(newItem);
+                comboBox.addItem(newItem);
             }
         }
     }
